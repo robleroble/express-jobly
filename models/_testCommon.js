3de2baadeffe2817b11cae44d002177faecf53ab
Nick <nsand3@gmail.com>
2021-09-16 23:04:20 -0700

    added beforeAll step for job.test for models

--- models/_testCommon.js
+++ models/_testCommon.js
@@ -15,7 +15,8 @@ async function commonBeforeAll() {
            ('c2', 'C2', 2, 'Desc2', 'http://c2.img'),
            ('c3', 'C3', 3, 'Desc3', 'http://c3.img')`);
 
-  await db.query(`
+  await db.query(
+    `
         INSERT INTO users(username,
                           password,
                           first_name,
@@ -24,10 +25,19 @@ async function commonBeforeAll() {
         VALUES ('u1', $1, 'U1F', 'U1L', 'u1@email.com'),
                ('u2', $2, 'U2F', 'U2L', 'u2@email.com')
         RETURNING username`,
-      [
-        await bcrypt.hash("password1", BCRYPT_WORK_FACTOR),
-        await bcrypt.hash("password2", BCRYPT_WORK_FACTOR),
-      ]);
+    [
+      await bcrypt.hash("password1", BCRYPT_WORK_FACTOR),
+      await bcrypt.hash("password2", BCRYPT_WORK_FACTOR),
+    ]
+  );
+
+  const resultsJobs = await db.query(`
+    INSERT INTO jobs (title, salary, equity, company_handle)
+    VALUES ('Job1', 100, '0.1', 'c1'),
+           ('Job2', 200, '0.2', 'c1'),
+           ('Job3', 300, '0', 'c1'),
+           ('Job4', NULL, NULL, 'c1')
+    RETURNING id`);
 }
 
 async function commonBeforeEach() {
@@ -41,7 +51,6 @@ async function commonAfterEach() {
 async function commonAfterAll() {
   await db.end();
 }
-
 
 module.exports = {
   commonBeforeAll,
